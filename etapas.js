c990edff0f04824999a605c31e37204595fbf9cc
padawandev <born4codex@gmail.com>
2021-12-10 16:20:14 -0300

    Create JSON with candidate data.

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'xxxxx.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url: '77222', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'xx.png', legenda: 'Prefeito'},
+                    {url: 'xxx.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'Querty',
+                vice: 'Zul',
+                fotos: [
+                    {url: 'xxx.png', legenda: 'Prefeito'},
+                    {url: 'xxx.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
