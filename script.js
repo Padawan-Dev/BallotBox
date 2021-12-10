3a50af7adf22eb65dfcc218cb3fdb31614196090
padawandev <born4codex@gmail.com>
2021-12-10 17:19:27 -0300

    Create keyboard element capture.

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ let descricao = document.querySelector('.tela--top--left-4');
 let aviso = document.querySelector('.tela--down');
 let fotosCandidatos = document.querySelector('.tela--top--right');
 let numeros = document.querySelector('.tela--top--left-3');
+
+function clicou(n){
+    alert("Clicou em "+n);
+}
+function branco(){
+    alert("Clicou em BRANCO!");
+}
+function corrige(){
+    alert("clicou em CORRIGE!");
+}
+function confirma(){
+    alert("clicou em CONFIRMA!");
+}
